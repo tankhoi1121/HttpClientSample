260be46e7f8507502faf835fc3ee5162f629aad0
ASUS <ASUS@DESKTOP-12KOKFE>
2020-03-25 16:09:01 +0700

    Get API Success

--- HttpClientSample/Program.cs
+++ HttpClientSample/Program.cs
@@ -5,6 +5,9 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Json;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 
 namespace HttpClientSample
 {
@@ -15,20 +18,70 @@ namespace HttpClientSample
         public string data { get; set; }
     }
 
+    [JsonObject(MemberSerialization.OptIn)]
+    class City
+    {
+        [JsonProperty("regionId")]
+        public string regionId { get; set; }
+        [JsonProperty("cityName")]
+        public string cityName { get; set; }
+        [JsonProperty("stateName")]
+        public string stateName { get; set; }
+
+        [JsonProperty("cityPopulation")]
+        public string cityPopulation { get; set; }
+
+        [JsonProperty("area")]
+        public string area { get; set; }
+
+        public static implicit operator City(JToken v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
     class Program
     {
+        static string CapText(Match m)
+        {
+            string x = m.ToString();
+            if (char.IsDigit(x[0]))
+            {
+                return "\"" + x[0] + x.Substring(1, x.Length - 1) + "\"";
+            }
+            return x;
+        }
         static async Task Main(string[] args)
         {
             var httpClient = new HttpClient();
 
-            var result = await httpClient.GetAsync("http://localhost:5000/api/values");
+            //var result = await httpClient.GetAsync("http://localhost:5000/api/values");
 
-            var content = await result.Content.ReadAsStringAsync();
+            HttpContent content1 = null;
 
-            var dict = new Dictionary<string, string>();
-            dict["text"] = content;
+            var result2 = await httpClient.PostAsync("http://localhost:5000/api/City/GetCity", content1);
 
-            Console.WriteLine(dict["text"]);
+            var content = result2.Content.ReadAsStringAsync().Result;
+
+            //var newItem = string.Concat(content.Split('[', ']', '{', '}'));
+
+
+            Regex regex = new Regex(@"[-+]?([0-9]*\.[0-9]+|[0-9]+)");
+
+            var s = regex.Replace(content, new MatchEvaluator(Program.CapText));
+
+
+            List<City> cityList = JsonConvert.DeserializeObject<List<City>>(s);
+
+            //var obj = cityList[0];
+
+            Console.WriteLine(result2.StatusCode);
+
+            //City realCity = (City)city;
+
+            //Console.WriteLine(s);
+
+            Console.WriteLine(cityList[0].cityName);
         }
     }
 }
